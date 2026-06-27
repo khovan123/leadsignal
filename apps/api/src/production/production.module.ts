75a10dea3e45fefa93247b12c520d93234f0823e
Phan Nguyen Quoc Minh <minhpnq1807@gmail.com>
2026-06-27 14:55:31 +0700

    fix(api): use explicit production provider factories

--- apps/api/src/production/production.module.ts
+++ apps/api/src/production/production.module.ts
@@ -1,5 +1,8 @@
 import { Global, Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
+import { CryptoService } from '../crypto/crypto.service';
+import { PrismaService } from '../database/prisma.service';
+import { QueueService } from '../queue/queue.service';
 import { InvitationsController } from './invitations.controller';
 import { OAuthController } from './oauth.controller';
 import { ProductionAuthGuard } from './production.guard';
@@ -8,11 +11,12 @@ import { SessionController } from './session.controller';
 
 @Global()
 @Module({
-  controllers: [SessionController, InvitationsController, OAuthController],
-  providers: [
-    ProductionService,
-    { provide: APP_GUARD, useClass: ProductionAuthGuard },
+  controllers:[SessionController,InvitationsController,OAuthController],
+  providers:[
+    {provide:ProductionService,useFactory:(prisma:PrismaService,crypto:CryptoService,queue:QueueService)=>new ProductionService(prisma,crypto,queue),inject:[PrismaService,CryptoService,QueueService]},
+    {provide:ProductionAuthGuard,useFactory:(prisma:PrismaService)=>new ProductionAuthGuard(prisma),inject:[PrismaService]},
+    {provide:APP_GUARD,useExisting:ProductionAuthGuard},
   ],
-  exports: [ProductionService],
+  exports:[ProductionService],
 })
 export class ProductionModule {}
