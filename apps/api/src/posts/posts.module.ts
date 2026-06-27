d5c3b44a98ff2defcb52d136ec024dcbc5467074
Phan Nguyen Quoc Minh <minhpnq1807@gmail.com>
2026-06-27 16:38:10 +0700

    fix posts controller wiring

--- apps/api/src/posts/posts.module.ts
+++ apps/api/src/posts/posts.module.ts
@@ -1,20 +1,8 @@
-import { Module } from '@nestjs/common';
-import { CqrsModule } from '@nestjs/cqrs';
-import { IngestPostHandler } from './application/ingest-post.command';
-import { REDDIT_POST_REPOSITORY } from './domain/reddit-post.repository';
-import { PrismaRedditPostRepository } from './infrastructure/prisma-reddit-post.repository';
-import { PostsController } from './presentation/posts.controller';
-
-@Module({
-  imports: [CqrsModule],
-  controllers: [PostsController],
-  providers: [
-    IngestPostHandler,
-    PrismaRedditPostRepository,
-    {
-      provide: REDDIT_POST_REPOSITORY,
-      useExisting: PrismaRedditPostRepository,
-    },
-  ],
-})
-export class PostsModule {}
+import {Module} from '@nestjs/common';
+import {CqrsModule} from '@nestjs/cqrs';
+import {IngestPostHandler} from './application/ingest-post.command';
+import {REDDIT_POST_REPOSITORY} from './domain/reddit-post.repository';
+import {PrismaRedditPostRepository} from './infrastructure/prisma-reddit-post.repository';
+import {PostsCqrsController} from './presentation/posts-cqrs.controller';
+@Module({imports:[CqrsModule],controllers:[PostsCqrsController],providers:[IngestPostHandler,PrismaRedditPostRepository,{provide:REDDIT_POST_REPOSITORY,useExisting:PrismaRedditPostRepository}]})
+export class PostsModule{}
