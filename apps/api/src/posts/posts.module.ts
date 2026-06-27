6be4ff3ed40431fdf36c43c10573c5529b0cd6ea
Phan Nguyen Quoc Minh <minhpnq1807@gmail.com>
2026-06-27 16:19:32 +0700

    refactor(posts): wire DDD CQRS module

--- apps/api/src/posts/posts.module.ts
+++ apps/api/src/posts/posts.module.ts
@@ -1,5 +1,20 @@
 import { Module } from '@nestjs/common';
-import { PostsController } from './posts.controller';
-import { PostsService } from './posts.service';
-@Module({ controllers: [PostsController], providers: [PostsService] })
+import { CqrsModule } from '@nestjs/cqrs';
+import { IngestPostHandler } from './application/ingest-post.command';
+import { REDDIT_POST_REPOSITORY } from './domain/reddit-post.repository';
+import { PrismaRedditPostRepository } from './infrastructure/prisma-reddit-post.repository';
+import { PostsController } from './presentation/posts.controller';
+
+@Module({
+  imports: [CqrsModule],
+  controllers: [PostsController],
+  providers: [
+    IngestPostHandler,
+    PrismaRedditPostRepository,
+    {
+      provide: REDDIT_POST_REPOSITORY,
+      useExisting: PrismaRedditPostRepository,
+    },
+  ],
+})
 export class PostsModule {}
