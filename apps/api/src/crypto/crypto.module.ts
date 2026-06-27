63c3940606b132ab5569d960f437e9a78664fb6d
Phan Nguyen Quoc Minh <minhpnq1807@gmail.com>
2026-06-27 14:07:27 +0700

    refactor(config): expose runtime secrets through global crypto module

--- apps/api/src/crypto/crypto.module.ts
+++ apps/api/src/crypto/crypto.module.ts
@@ -1,6 +1,7 @@
 import { Global, Module } from '@nestjs/common';
+import { SecretsService } from '../secrets/secrets.service';
 import { CryptoService } from './crypto.service';
 
 @Global()
-@Module({ providers: [CryptoService], exports: [CryptoService] })
+@Module({ providers: [SecretsService, CryptoService], exports: [SecretsService, CryptoService] })
 export class CryptoModule {}
