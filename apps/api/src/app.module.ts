729cf063388331f31e9789e21b9db9a39a9c8153
Phan Nguyen Quoc Minh <minhpnq1807@gmail.com>
2026-06-27 16:29:59 +0700

    refactor(api): add identity context

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -8,8 +8,9 @@ import { LeadsModule } from './leads/leads.module';
 import { QueueModule } from './queue/queue.module';
 import { PostsModule } from './posts/posts.module';
 import { ProductionModule } from './production/production.module';
+import { IdentityModule } from './identity/identity.module';
 
 @Module({
-  imports: [DatabaseModule, CryptoModule, HealthModule, WorkspacesModule, LlmModule, LeadsModule, QueueModule, PostsModule, ProductionModule],
+  imports: [DatabaseModule, CryptoModule, HealthModule, WorkspacesModule, LlmModule, LeadsModule, QueueModule, PostsModule, ProductionModule, IdentityModule],
 })
 export class AppModule {}
