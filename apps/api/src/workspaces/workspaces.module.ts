cafd95e71913d6db1a3695aa8b1f100f97ea1689
Phan Nguyen Quoc Minh <minhpnq1807@gmail.com>
2026-06-27 16:47:16 +0700

    fix workspace adapter DI

--- apps/api/src/workspaces/workspaces.module.ts
+++ apps/api/src/workspaces/workspaces.module.ts
@@ -1,8 +1,9 @@
 import {Module} from '@nestjs/common';
 import {CqrsModule} from '@nestjs/cqrs';
+import {PrismaService} from '../database/prisma.service';
 import {GetWorkspaceHandler} from './application/get-workspace.query';
 import {WORKSPACE_REPOSITORY} from './domain/workspace.repository';
 import {PrismaWorkspaceRepository} from './infrastructure/prisma-workspace.repository';
 import {WorkspacesCqrsController} from './presentation/workspaces-cqrs.controller';
-@Module({imports:[CqrsModule],controllers:[WorkspacesCqrsController],providers:[GetWorkspaceHandler,PrismaWorkspaceRepository,{provide:WORKSPACE_REPOSITORY,useExisting:PrismaWorkspaceRepository}]})
+@Module({imports:[CqrsModule],controllers:[WorkspacesCqrsController],providers:[GetWorkspaceHandler,{provide:PrismaWorkspaceRepository,useFactory:(prisma:PrismaService)=>new PrismaWorkspaceRepository(prisma),inject:[PrismaService]},{provide:WORKSPACE_REPOSITORY,useExisting:PrismaWorkspaceRepository}]})
 export class WorkspacesModule{}
